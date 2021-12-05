a08ec7544da09e672daa7bf9c28deb74324f4b41
DragostinH <dragostin.hristov@gmail.com>
2021-12-05 19:32:56 +0200

    Switch works!

--- BeginningOfJS/firstDemoOfJS/app.js
+++ BeginningOfJS/firstDemoOfJS/app.js
@@ -1,1 +1,19 @@
 console.log("Hello boiz");
+
+// if (2 % 2 === 0) {
+//     console.log("even");
+// }
+
+switch (5 % 2) {
+    case 0:
+        console.log("even");
+        break;
+
+    case 1:
+        console.log("odd");
+        break;
+
+    default:
+        console.log("its not either even or odd")
+        break;
+}
