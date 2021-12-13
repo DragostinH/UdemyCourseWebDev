632de59e07c257e9bb16e1f927985aee21af7296
DragostinH <dragostin.hristov@gmail.com>
2021-12-13 22:38:18 +0200

    Practicing stuff in JS

---
+++ JS/Loops/script.js
@@ -0,0 +1,31 @@
+console.log("test");
+
+for(let i = 0; i<= 10; i++){
+    console.log(i);
+    for(let j = 10; j>=0; j--){
+        console.log(j);
+    }
+}
+
+
+console.log("this is a test");
+
+let a = "stawsra";
+
+a.toU
+
+
+const shoppingCard = {
+    itemName: "chushki",
+    quantity: 2,
+    type: ["red", "green"],
+}
+
+
+const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"]; //DONT TOUCH THIS LINE!
+
+// WRITE YOUR LOOP BELOW THIS LINE:
+
+for(let i = 0; i < people.length; i++){
+    console.log(people[i].toUpperCase());
+}
